d17912411422bf5e2258e306acd8d03c15d6f16c
Kostiantyn Babich (Connie) <babich.kostia@gmail.com>
2024-05-28 18:43:46 -0700

    Message is package-private

--- src/server/Message.java
+++ src/server/Message.java
@@ -2,7 +2,7 @@ package server;
 
 import org.json.JSONObject;
 
-public class Message {
+class Message {
     private final String content;
     private final String sender;
     private final String timeStamp;
